b71d624e15dbea8221dc4e92a7944a26f0d57b3e
Adam Buechler <butchler@gmail.com>
2016-10-01 22:47:48 +0900

    Use mocha reporter.

--- karma.conf.js
+++ karma.conf.js
@@ -19,6 +19,7 @@ module.exports = function(config) {
       require('karma-sinon'),
       require('karma-phantomjs-launcher'),
       require('karma-sourcemap-loader'),
+      require('karma-mocha-reporter'),
     ],
 
 
@@ -54,7 +55,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['mocha'],
 
 
     // web server port
